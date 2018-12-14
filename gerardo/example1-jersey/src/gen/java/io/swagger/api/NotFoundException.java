779d748debe7ec98d33880d244adc7f8434df41e
grada84 <gerardorada84@gmail.com>
2018-12-14 10:42:57 -0300

    opentech

--- gerardo/example1-jersey/src/gen/java/io/swagger/api/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package io.swagger.api;
-
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJerseyServerCodegen", date = "2018-12-12T14:47:39.534Z[GMT]")public class NotFoundException extends ApiException {
-    private int code;
-    public NotFoundException (int code, String msg) {
-        super(code, msg);
-        this.code = code;
-    }
-}
